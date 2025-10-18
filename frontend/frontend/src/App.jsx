89f18385c2ac104ee70ca43cb118badb998eff28
AHTupoB-AHDpeU <antipov13.ya.ru@mail.ru>
2025-10-19 00:07:15 +0300

    frontend + backend

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,26 @@
+﻿import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
+function App() {
+    return (
+        <Router>
+            <div style={{ display: "flex", justifyContent: "center", gap: "20px", marginTop: "20px" }}>
+                <Link to="/">
+                    <button>Главная страница</button>
+                </Link>
+                <Link to="/about">
+                    <button>О нас</button>
+                </Link>
+            </div>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
---
+++ frontend/frontend/src/Home.jsx
@@ -0,0 +1,11 @@
+﻿import React from "react";
+
+function Home() {
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "80vh", fontSize: "32px" }}>
+            Главная страница
+        </div>
+    );
+}
+
+export default Home;
